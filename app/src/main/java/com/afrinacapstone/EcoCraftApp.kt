2fbcdd4c2aa9736657742d41eb10ecc8f8977b73
Afrina Pasaribu <afrinapasaribu36@gmail.com>
2024-12-12 23:40:50 +0700

    update repo

--- app/src/main/java/com/afrinacapstone/EcoCraftApp.kt
+++
@@ -1,8 +0,0 @@
-package com.afrinacapstone
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class EcoCraftApp : Application() {
-}
--- app/src/main/java/com/afrinacapstone/ecocraft/util/Helper.kt
+++
@@ -1,9 +0,0 @@
-package com.afrinacapstone.ecocraft.util
-
-import com.google.gson.Gson
-import okhttp3.ResponseBody
-
-inline fun <reified T> parseResponse(responseBody: ResponseBody): T {
-    val gson = Gson()
-    return gson.fromJson(responseBody.charStream(), T::class.java)
-}
